7ff18c624a3d3b2852279ff90fd66f0ccca5753f
aqgoesdf <aqgoesdf@gmail.com>
2026-05-05 20:13:07 -0300

    Criando superusuários

--- core/views.py
+++ core/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
 # Editado por aparti daqui.
+from .models import Post
+
 def home(request):
-    return render(request, 'core/base.html')
+    posts = Post.objects.all().order_by('-data_criacao')
+    destaque = posts.first()  # primeiro post como destaque
+    outros_posts = posts[1:]  # restante
+
+    return render(request, 'core/home.html', {
+        'posts': outros_posts,
+        'destaque': destaque
+    })
+
