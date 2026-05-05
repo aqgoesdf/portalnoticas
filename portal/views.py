9fcaee1d3655ac11c0389f649be96d2397ba6428
aqgoesdf <aqgoesdf@gmail.com>
2026-05-05 16:54:11 -0300

    Estrutura django-portal

--- portal/views.py
+++ portal/views.py
@@ -1,18 +1,6 @@
 from django.shortcuts import render
-from .models import Noticia
 
 def index(request):
-    destaque = Noticia.objects.filter(destaque=True).first()
-    secundarias = Noticia.objects.filter(destaque=False)[:3]
-    ultimas = Noticia.objects.order_by('-criado_em')[:5]
-
-
-    context = {
-        'destaque': destaque,
-        'secundarias': secundarias,
-        'ultimas': ultimas
-    }
-
-    return render(request, 'portal/index.html', context)
+    return render(request, 'portal/home.html')
 
 # Create your views here.
